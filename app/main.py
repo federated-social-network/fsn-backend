b602551bee100b89751cc622ee2c91f2e9fa7723
a-anuj <anujamirthalingam5@gmail.com>
2025-12-24 18:51:07 +0530

    feat : add endpoint to create new posts

--- app/main.py
+++ app/main.py
@@ -2,6 +2,32 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.models import Post
 
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from app.database import SessionLocal
+from app.config import settings
+
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Federated Backend")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/posts")
+def create_post(content:str,author:str,db:Session=Depends(get_db)):
+    post = Post(
+        content = content,
+        author=author,
+        origin_instance=settings.INSTANCE_NAME,
+        is_remote=False
+    )
+    db.add(post)
+    db.commit()
+    db.refresh(post)
+    return post
