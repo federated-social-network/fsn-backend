7fda8f44568979b18c0ebb1902dc728c2b34636a
a-anuj <anujamirthalingam5@gmail.com>
2026-02-08 18:08:47 +0530

    chore : add another test case for login endpoint

--- tests/auth/test_login.py
+++ tests/auth/test_login.py
@@ -5,3 +5,13 @@ def test_user_login(client):
     })
     print(response.json())
     assert response.status_code == 200
+
+def test_user_login_accessToken(client):
+    response = client.post('/auth/login', params={
+        "username": "newuser13",
+        "password": "newpassword1"
+    })
+    print(response.json())
+    assert "access_token" in response.json()
+    assert response.status_code == 200
+
