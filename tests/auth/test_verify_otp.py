53b3e54bd3718fb5fe566ad81d0a9c7a43270590
a-anuj <anujamirthalingam5@gmail.com>
2026-02-09 09:37:22 +0530

    feat : add tests for verify otp

---
+++ tests/auth/test_verify_otp.py
@@ -0,0 +1,6 @@
+def test_verify_otp_failure(client):
+    response = client.post("/auth/verify-otp", json={
+        "otp": "123456",
+        "email": "example12@gmail.com"
+        })
+    assert response.status_code == 400
