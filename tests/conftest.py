e38ddce1509ef8b9417e1cbc0736dd6ea17e7b73
a-anuj <anujamirthalingam5@gmail.com>
2026-02-09 10:13:42 +0530

    fix : update the fake_user function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,39 @@
+from fastapi import Depends
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app
 from app.routers.users import get_current_user
 from app.models import User
+import uuid
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.models import Post, User
 
 @pytest.fixture
 def client():   
     return TestClient(app)
 
 @pytest.fixture
-def fake_user():
-    return User(
-        id="user-123",
+def db():
+    db = next(get_db())
+    try:
+        yield db
+    finally:
+        db.close()
+
+@pytest.fixture
+def fake_user(db: Session):
+    user = User(
+        id=str(uuid.uuid4()),
         username="testuser",
-        email="test@test.com"
+        email="test@test.com",
+        password_hash=User.hash_password("testpassword")
     )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
 
 @pytest.fixture(autouse=True)
 def override_auth(fake_user):
