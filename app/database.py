57ee12ce27bdca311de326377a7a9e3823a8a1d6
a-anuj <anujamirthalingam5@gmail.com>
2026-02-27 00:09:27 +0530

    fix : change the logic of creating the databases

--- app/database.py
+++ app/database.py
@@ -1,16 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import settings
+from sqlalchemy.pool import NullPool
 
 
 engine = create_engine(
     settings.DATABASE_URL,
-    connect_args={"sslmode": "require", "options": "-c statement_timeout=5000"},
-    pool_size=20,
-    max_overflow=40,
-    pool_timeout=30,
-    pool_recycle=1800,
-    pool_pre_ping=True,
+    connect_args={
+        "sslmode": "require",
+        "options": "-c statement_timeout=5000",
+    },
+    poolclass=NullPool,
 )
 
 
