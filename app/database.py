5d66a68ba420c7a0cc2692c9d6143a4e5b2009de
a-anuj <anujamirthalingam5@gmail.com>
2026-02-27 00:44:57 +0530

    fix : update database.py

--- app/database.py
+++ app/database.py
@@ -1,20 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.config import settings
 from sqlalchemy.pool import NullPool
-
+from app.config import settings
 
 engine = create_engine(
     settings.DATABASE_URL,
-    connect_args={
-        "sslmode": "require",
-        "options": "-c statement_timeout=5000",
-    },
+    connect_args={"sslmode": "require"},
     poolclass=NullPool,
 )
 
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
 
-SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
 
