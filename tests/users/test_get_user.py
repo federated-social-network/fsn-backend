45e2093c427bf12f25d47e535f785de90f210d98
a-anuj <anujamirthalingam5@gmail.com>
2026-03-07 19:50:38 +0530

    test : write unit tests for APIs in users router

---
+++ tests/users/test_get_user.py
@@ -0,0 +1,12 @@
+def test_get_user(client):
+    client.post("auth/register", params={"username": "testuser", "password": "testpass","email":"test@test.com"})
+    response = client.get("/get_user/testuser")
+    assert response.status_code == 200
+
+def test_get_user_not_found(client):
+    response = client.get("/get_user/nonexistentuser")
+    assert response.status_code == 404
+
+def test_random_users(client):
+    response = client.get("/random_users/")
+    assert response.status_code == 200
